36306d7f7d9e119f07eaab08fcb0a5c5c3d2ad27
ganna.kolesnyk <annet.k93@gmail.com>
2024-04-30 20:26:56 +0300

    update file test_task1.py

--- test_task1.py
+++ test_task1.py
@@ -6,7 +6,24 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-def test_search():
+def test_search_Firefox_br():
+    browser = webdriver.Firefox()
+    browser.maximize_window()
+    browser.get('https://google.com/')
+    consent = browser.find_element(By.ID, 'W0wltc')
+    consent.click()
+    search_field = browser.find_element(By.ID, 'APjFqb')
+    search_field.clear()
+    search_field.send_keys('samsung galaxy')
+    search_field.send_keys(Keys.RETURN)
+    time.sleep(7)
+
+    elems = browser.find_elements(By.CSS_SELECTOR, "div#search div.yuRUbf a")
+    elems[10].click()
+    time.sleep(5)
+    browser.close()
+
+def test_search_Chorme_br():
     browser = webdriver.Firefox()
     browser.maximize_window()
     browser.get('https://google.com/')
